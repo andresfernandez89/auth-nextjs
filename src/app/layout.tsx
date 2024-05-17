e7facfb28e969360789e00032a962d75b8aa8314
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-05-17 08:18:03 -0300

    feat: add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "dark min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
         )}
       >
